224328583344a64cbfe47836230a54359346be7b
hakozaru <t.morii@everyleaf.com>
2021-03-07 09:31:08 +0900

    演習6-2終了

--- src/index.js
+++ src/index.js
@@ -6,21 +6,17 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const dispatchStateFor = type => store.dispatch({ type })
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => dispatchStateFor('GOOD')}>good</button>
+      <button onClick={() => dispatchStateFor('OK')}>neutral</button>
+      <button onClick={() => dispatchStateFor('BAD')}>bad</button>
+      <button onClick={() => dispatchStateFor('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -8,14 +8,13 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      const newState = { ...state, good: state.good + 1 }
-      return newState
+      return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
-      return state
+      return initialState
     default: return state
   }
 
